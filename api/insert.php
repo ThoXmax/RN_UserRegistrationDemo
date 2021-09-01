5fabfd12f133ac2c7786c357fed1d207c796d3a1
max <85299566+ThoXmax@users.noreply.github.com>
2021-09-01 08:13:00 +0800

    fix: add encoded data and decoded data in the insert API

--- api/insert.php
+++ api/insert.php
@@ -2,9 +2,12 @@
     $CN = mysqli_connect("localhost", "root", "");
     $DB = mysqli_select_db($CN, "member");
 
-    $m_no = $_POST['m_no'];
-    $m_name = $_POST['m_name'];
-    $m_course = $_POST['m_course'];
+    $EncodedData = file_get_contents('php://input');
+    $DecodedData = json_decode($EncodedData, true);
+
+    $m_no = $DecodedData['m_no'];
+    $m_name = $DecodedData['m_name'];
+    $m_course = $DecodedData['m_course'];
 
     $insertMemberData = "insert into memberData(m_no, m_name, m_course) values ($m_no, '$m_name', '$m_course')";
 
